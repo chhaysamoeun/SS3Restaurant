cd8de5e2a122535637b74382e9c450371815f848
Samoeun <samoeunchh@gmail.com>
2023-01-06 16:59:10 +0700

    Category and Customer

---
+++ SS3Restaurant/Models/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace SS3Restaurant.Models
+{
+	public class AppDbContext:DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options)
+			:base(options)
+		{
+		}
+		public DbSet<Category> Category { get; set; }
+		public DbSet<Customer> Customer { get; set; }
+	}
+}
+
