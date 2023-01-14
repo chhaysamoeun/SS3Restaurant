428eded6524124567cb05edc4fba22dc27427700
Samoeun <samoeunchh@gmail.com>
2023-01-14 14:14:54 +0700

    Sale Form

---
+++ SS3Restaurant/Controllers/SaleController.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SS3Restaurant.Models;
+
+namespace SS3Restaurant.Controllers
+{
+    public class SaleController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public SaleController(AppDbContext context)
+        {
+            _context = context;
+        }
+        // GET: Sale
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public async Task<JsonResult> GetProductByCategory(Guid Id)
+            => Json(await _context.Product.Where(x => x.CategoryId.Equals(Id)).ToListAsync());
+        // GET: Sale/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Sale/Create
+        public async Task<ActionResult> Create()
+        {
+            ViewData["Category"] = await _context.Category.ToListAsync();
+            ViewData["Tables"] = await _context.Tables.ToListAsync();
+            ViewData["Customer"] = new SelectList(_context.Customer, "CustomerId", "CustomerName");
+            return View();
+        }
+
+        // POST: Sale/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Sale/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Sale/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Sale/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Sale/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SS3Restaurant/Models/Sale.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SS3Restaurant.Models
+{
+	public class Sale
+	{
+		[Key]
+		public Guid SaleId { get; set; }
+		[ForeignKey("Customer")]
+		public Guid CustomerId { get; set; }
+        [ForeignKey("Tables")]
+        public Guid TableId { get; set; }
+		public DateTime IssueDate { get; set; }
+		[MaxLength(20)]
+		public string InvoiceNumber { get; set; }
+		public double Total { get; set; }
+		public int Discount { get; set; }
+		public double GrandTotal { get; set; }
+		public bool IsHold { get; set; }
+
+		public Customer Customer { get; set; }
+		public Tables Tables { get; set; }
+	}
+}
+
